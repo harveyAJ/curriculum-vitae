8d603d7fec157bd9c1d2202323816d90bb765888
Valentin A Roy <valentin.a.roy@collins.com>
2025-11-23 21:36:05 +0000

    update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -35,19 +35,64 @@ resource "cloudflare_pages_project" "cv" {
   }
 }
 
-# TODO add a CNAME for www here too?
-resource "cloudflare_dns_record" "cv_dns" {
-  zone_id = data.sops_file.cf_secrets.data["zone_id"]
-  name    = "@"
-  type    = "A"
-  content = "192.0.2.1"
-  ttl     = 1
-  proxied = true
-}
-
 # Bind the domain to the Pages project
+# This will generate the CNAME target e.g. cv-valentin-abc.pages.dev
 resource "cloudflare_pages_domain" "cv_domain" {
   account_id      = data.sops_file.cf_secrets.data["account_id"]
   project_name    = cloudflare_pages_project.cv.name
   name            = var.domain
+}
+
+locals {
+  pages_cname_target = cloudflare_pages_project.cv.subdomain
+}
+
+# Apex domain
+resource "cloudflare_dns_record" "apex" {
+  zone_id = data.sops_file.cf_secrets.data["zone_id"]
+  name    = var.domain
+  type    = "CNAME"
+  content = local.pages_cname_target
+  ttl     = 1
+  proxied = true
+}
+
+# www domain
+resource "cloudflare_dns_record" "www" {
+  zone_id = data.sops_file.cf_secrets.data["zone_id"]
+  name    = "www"
+  type    = "CNAME"
+  content = local.pages_cname_target
+  ttl     = 1
+  proxied = true
+}
+
+# Redirect www.valentin-roy.dev to https://valentin-roy.dev
+# A redirect is recommended so search engine treat these two as one site (SEO best practices)
+resource "cloudflare_ruleset" "www_to_root_redirect" {
+  zone_id = data.sops_file.cf_secrets.data["zone_id"]
+  name    = "Redirect www to root"
+  kind    = "zone"
+  phase   = "http_request_redirect"
+
+  rules = [{
+    ref = "redirect_www_to_root"
+    action = "redirect"
+    description = "Redirect www to root"
+    enabled = true
+
+    # Match only the www subdomain
+    expression = "(http.host eq \"www.${var.domain}\")"
+
+    action_parameters = {
+      from_value = {
+        status_code = 301
+        target_url  = {
+          value = "https://${var.domain}${"$${uri}"}"
+        }
+        preserve_query_string = true #ensures ?a=b stays attached
+      }
+    }
+  }
+  ]
 }
